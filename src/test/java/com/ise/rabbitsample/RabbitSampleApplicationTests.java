6bd3c8d678dfd06ed9ac9a442a69b74186bbab42
ise-208 <seed.0ri00@gmail.com>
2023-06-02 18:50:30 +0900

    first commit

---
+++ src/test/java/com/ise/rabbitsample/RabbitSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ise.rabbitsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
